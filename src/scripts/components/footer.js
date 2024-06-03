d06befdc62883ffd70bd48cfcba91ce7d9a62e60
Bintang\Admin <mbintangmandala237956@gmail.com>
2024-06-03 20:41:26 +0700

    menambahkan kode footer

---
+++ src/scripts/components/footer.js
@@ -0,0 +1,25 @@
+class footerBar extends HTMLElement {
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <div class="container">
+            <div class="foot-title">About Us</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <div class="social-media">
+                <div class="social-media-item"><i class="fa-brands fa-facebook-f"></i></div>
+                <div class="social-media-item"><i class="fa-brands fa-twitter"></i></div>
+                <div class="social-media-item"><i class="fa-brands fa-instagram"></i></div>
+                <div class="social-media-item"><i class="fa-brands fa-youtube"></i></div>
+            </div>
+        </div>
+        <div class="foot">
+            <p>YGT &#169; 2022, YGTreasure</p>
+        </div>
+        `;
+    }
+}
+
+customElements.define('footer-bar', footerBar);
