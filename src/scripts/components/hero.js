8510fab1b158592659fbe7422ae7428a26201705
LAPTOP-KCETHLSE\LENOVO <aiman.nurhakim117@gmail.com>
2024-06-04 19:01:51 +0700

    Merapihkan code dan css

--- src/scripts/components/hero.js
+++ src/scripts/components/hero.js
@@ -5,12 +5,13 @@ class Hero extends HTMLElement {
 
   render() {
     this.innerHTML = `
-            <div class="hero">
-                <div class="hero__desc">
-                    <h1 class="hero__title">Halo, Petualang</h1>
+      <div class="hero">
+      <div class='overlay'></div>
+      <div class="hero__desc">
+                <h1 class="hero__title">Halo, Petualang</h1>
+                    <button type="button" id="explore-btn">Explore</button>
                     <p class="hero__tagline">Selamat datang di YGT, panduan wisata terpercaya Anda untuk menjelajahi keindahan Yogyakarta, kota budaya dan sejarah yang kaya. Kami sangat senang Anda bergabung dengan komunitas petualang kami yang penuh semangat. Di sini, Anda akan menemukan berbagai informasi menarik, rekomendasi tempat wisata terbaik, kuliner lokal yang menggugah selera, dan tips perjalanan yang bermanfaat untuk membuat pengalaman Anda semakin berkesan.
                     </p>
-                    <button type="button" id="explore-btn">Explore</button>
                 </div>
             </div>
           `;
