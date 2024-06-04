e82196c18d686fa5c1c3befd49f1e590b2d4a7a7
Frdyan <ferdiharyadi123@gmail.com>
2024-06-04 17:15:40 +0700

    webpack

---
+++ src/scripts/components/header.js
@@ -0,0 +1,25 @@
+class headBar extends HTMLElement {
+    connectedCallback() {
+      this.render();
+    }
+  
+    render() {
+      this.innerHTML = `
+          <div class="head-bar">
+            <a class="logo" href="" tabindex="0">
+              <h1 class="title">YG Treasure</h1>
+            </a>
+            <button type="button" id="hamburger" aria-label="navigation-menu" tabindex="0">☰</button>
+            <nav id="drawer">
+              <ul class="nav-list" id="navigation">
+                <li class="nav-item"><a href="#">Home</a></li>
+                <li class="nav-item"><a href="#">Content</a></li>
+                <li class="nav-item"><a href="#">About Us</a></li>
+              </ul>
+            </nav>
+            
+          </div>
+          `;
+    }
+  }
+  customElements.define('head-bar', headBar);
---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+import 'regenerator-runtime';
+import './components/header';
+import './components/hero';
+import './components/footer';
+
+console.log('Hello Coders!');
