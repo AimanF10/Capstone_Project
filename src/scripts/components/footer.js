0e32dbff00bb087cff4af771d8367e86ac9fc18f
Bintang\Admin <mbintangmandala237956@gmail.com>
2024-06-05 15:19:26 +0700

    menambah nama anggota

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -10,12 +10,20 @@ class footerBar extends HTMLElement {
       <div class="foot-title">About Us</div>
           <p>Yogyakarta Treasures hadir untuk mengungkap "hidden gems" wisata di Daerah Istimewa Yogyakarta.<br> Kami memberikan solusi inovatif bagi wisatawan yang mencari pengalaman unik dan autentik,<br> sambil mendukung ekonomi lokal dan pelestarian lingkungan. Temukan keindahan tersembunyi<br> Daerah Istimewa Yogyakarta bersama kami dan nikmati perjalanan yang tak terlupakan.</p>
           </div>
-          <div class="social-media">
-              <div class="social-media-item"><i class="fab fa-facebook-f"></i></div>
-              <div class="social-media-item"><i class="fab fa-twitter"></i></div>
-              <div class="social-media-item"><i class="fab fa-instagram"></i></div>
-              <div class="social-media-item"><i class="fab fa-youtube"></i></div>
-          </div>
+      </div>
+      <div class="team">
+      <div class="foot-title">Our Team</div>
+      <div class="team-member">
+        <h3>Member 1</h3>
+        <p>Deskripsi singkat tentang anggota tim pertama. Misalnya, latar belakang, peran di tim, dan keahlian.</p>
+      </div>
+      <div class="team-member">
+        <h3>Member 2</h3>
+        <p>Deskripsi singkat tentang anggota tim kedua. Misalnya, latar belakang, peran di tim, dan keahlian.</p>
+      </div>
+      <div class="team-member">
+        <h3>Member 3</h3>
+        <p>Deskripsi singkat tentang anggota tim ketiga. Misalnya, latar belakang, peran di tim, dan keahlian.</p>
       </div>
       <div class="foot">
           <p> YGT &#169; 2024, YGTreasure</p>
